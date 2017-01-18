fb804bbd7ecae552519b8c893390dc92eb595c52
Anubis Lockward <anubis.lockward@gmail.com>
2017-01-18 17:27:54 -0400

    Override getCharge() from Price to replace Conditional

--- src/movierental/NewReleasePrice.java
+++ src/movierental/NewReleasePrice.java
@@ -6,5 +6,9 @@ public class NewReleasePrice extends Price {
 	int getPriceCode() {		
 		return Movie.NEW_RELEASE;
 	}
+	
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
 
 }
